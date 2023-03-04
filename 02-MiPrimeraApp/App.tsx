0eecf676d3e2bcfee78eaf887f81f6f37a5cc409
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-03-04 00:04:00 -0500

    MiPrimeraApp, hola mundo

---
+++ 02-MiPrimeraApp/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text style={{
+        fontSize: 55
+      }}>
+        Hola mundo
+      </Text>
+    </View>
+  )
+}
+
+export default App;
