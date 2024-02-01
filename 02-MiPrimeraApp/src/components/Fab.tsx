969dc8acc778d54fe20d1b1b3027c77535c2cb96
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-01-31 20:56:27 -0500

    ios and android Fab

--- 02-MiPrimeraApp/src/components/Fab.tsx
+++ 02-MiPrimeraApp/src/components/Fab.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  View,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 interface Props {
   title: string;
@@ -8,21 +15,39 @@ interface Props {
 }
 
 export const Fab = ({title, onPress, position}: Props) => {
-  return (
-    <View
+  const ios = () => {
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => onPress(-1)}
       style={[
         styles.fabLocation,
         position === 'br' ? styles.right : styles.left,
       ]}>
-      <TouchableNativeFeedback
-        onPress={() => onPress(-1)}
-        background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>{title}</Text>
-        </View>
-      </TouchableNativeFeedback>
-    </View>
-  );
+      <View style={styles.fab}>
+        <Text style={styles.fabText}>{title}</Text>
+      </View>
+    </TouchableOpacity>;
+  };
+
+  const android = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'br' ? styles.right : styles.left,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={() => onPress(-1)}
+          background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? ios() : android();
 };
 
 const styles = StyleSheet.create({
