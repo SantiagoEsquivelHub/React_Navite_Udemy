3122007802af7b0deceeef46b4813b504c9cce29
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-02-12 17:38:36 -0500

    Padding, Margin, Border, Width y Height

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
-import {CounterScreen} from './src/screens/CounterScreen';
+import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
+import { CounterScreen } from './src/screens/CounterScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { SafeAreaView } from 'react-native';
 
 export const App = () => {
   return (
-    // <HelloWorldScreen />
-    <CounterScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };
