f8933047eb159d7a816a575821d407ca7b834b7f
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-02-12 19:46:21 -0500

    Absolutve position

--- 02-MiPrimeraApp/src/screens/PositionScreen.tsx
+++ 02-MiPrimeraApp/src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, View } from 'react-native'
 export const PositionScreen = () => {
     return (
         <View style={styles.container}>
+            <View style={styles.greenBox}></View>
             <View style={styles.purpleBox}></View>
             <View style={styles.orangeBox}></View>
         </View>
@@ -15,8 +16,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#38CDD9',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // width: 400,
+        // height: 400,
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
     purpleBox: {
         width: 100,
@@ -24,12 +27,29 @@ const styles = StyleSheet.create({
         backgroundColor: '#5856D6',
         borderWidth: 10,
         borderColor: 'white',
+        position: 'absolute',
+        right: 0,
     },
     orangeBox: {
         width: 100,
         height: 100,
         backgroundColor: '#F0A23B',
         borderWidth: 10,
-        borderColor: 'white',
+        borderColor: 'white', 
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+    },
+    greenBox: {
+        // width: 100,
+        // height: 100,
+        backgroundColor: 'green',
+        borderWidth: 10,
+        borderColor: 'white', 
+        position: 'absolute',
+        bottom: 0,
+        top: 0,
+        left: 0,
+        right: 0,
     },
 })
