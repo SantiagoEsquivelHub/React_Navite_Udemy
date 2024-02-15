cbfb3b0fa70c70f77173c997d484f1db6f412aa4
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-02-15 08:11:19 -0500

    TaskScreen preparation

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
-import { CounterScreen } from './src/screens/CounterScreen';
-import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 import { SafeAreaView } from 'react-native';
-import { DimensionsScreen } from './src/screens/DimensionsScreen';
-import { PositionScreen } from './src/screens/PositionScreen';
-import { FlexScreen } from './src/screens/FlexScreen';
+// import { HelloWorldScreen } from './src/screens/HelloWorldScreen';
+// import { CounterScreen } from './src/screens/CounterScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { DimensionsScreen } from './src/screens/DimensionsScreen';
+// import { PositionScreen } from './src/screens/PositionScreen';
+// import { FlexScreen } from './src/screens/FlexScreen';
+import { TaskScreen } from './src/screens/TaskScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FlexScreen />
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: '#28425B'
+    }}>
+      <TaskScreen />
     </SafeAreaView>
   );
 };
