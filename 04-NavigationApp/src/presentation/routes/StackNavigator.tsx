f54282ff8ff1befd7ff12e500b77bcd16f20e6f7
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-03-29 21:12:35 -0500

    Styling Stack Navigator Header

--- 04-NavigationApp/src/presentation/routes/StackNavigator.tsx
+++ 04-NavigationApp/src/presentation/routes/StackNavigator.tsx
@@ -1,15 +1,22 @@
 import {createStackNavigator} from '@react-navigation/stack';
 
-import { SettingsScreen } from '../screens/settings/SettingsScreen';
-import { ProductsScreen } from '../screens/products/ProductsScreen';
-import { ProfileScreen } from '../screens/profile/ProfileScreen';
-import { HomeScreen } from '../screens/home/HomeScreen';
+import {SettingsScreen} from '../screens/settings/SettingsScreen';
+import {ProductsScreen} from '../screens/products/ProductsScreen';
+import {ProfileScreen} from '../screens/profile/ProfileScreen';
+import {HomeScreen} from '../screens/home/HomeScreen';
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerStyle: {
+          elevation: 0, // ios
+          shadowColor: 'transparent', // ios
+        },
+      }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductsScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
