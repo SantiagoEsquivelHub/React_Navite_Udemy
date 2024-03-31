4fa1b0e9eadbf671c993e7f22a945434e26616d3
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-03-30 23:31:40 -0500

    goBack and popTotop using navigator

--- 04-NavigationApp/src/presentation/screens/settings/SettingsScreen.tsx
+++ 04-NavigationApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,20 @@
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {StackActions, useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10}}>SettingsScreen</Text>
+
+      <PrimaryButton label="Go back" onPress={() => navigator.goBack()} />
+      <PrimaryButton
+        label="Go to home"
+        onPress={() => navigator.dispatch(StackActions.popToTop)}
+      />
     </View>
   );
 };
