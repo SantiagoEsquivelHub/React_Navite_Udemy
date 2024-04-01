0f5b4c7ebbfd1c424cd73400bd9315f4a279ef74
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-03-31 23:48:12 -0500

    Fix Drawer's problems

--- 04-NavigationApp/src/App.tsx
+++ 04-NavigationApp/src/App.tsx
@@ -1,12 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
-import { StackNavigator } from './presentation/routes/StackNavigator';
+// import {StackNavigator} from './presentation/routes/StackNavigator';
+import {SideMenuNavigator} from './presentation/routes/SideMenuNavigator';
 
 import 'react-native-gesture-handler';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideMenuNavigator />
     </NavigationContainer>
   );
 };
