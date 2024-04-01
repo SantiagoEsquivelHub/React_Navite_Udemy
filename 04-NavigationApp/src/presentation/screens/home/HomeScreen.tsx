37ec35f76987ae2f0e6e10e74e50f50a9bf95da2
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-03-31 23:52:12 -0500

    Hide and Show Toggle Drawer

--- 04-NavigationApp/src/presentation/screens/home/HomeScreen.tsx
+++ 04-NavigationApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,27 @@
-import {View} from 'react-native';
+import {useEffect} from 'react';
+import {View, Pressable, Text} from 'react-native';
 import {globalStyles} from '../../theme/theme';
-import {type NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  type NavigationProp,
+  useNavigation,
+  DrawerActions,
+} from '@react-navigation/native';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
 import {type RootStackParam} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParam>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menu</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
 
   return (
     <View style={globalStyles.container}>
