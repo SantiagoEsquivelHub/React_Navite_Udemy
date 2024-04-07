9b7ae1dc2d9a19d9e98cbf502b1110bfd71beec4
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-04-07 02:16:33 -0500

    Install icons dependencies

--- 04-NavigationApp/src/presentation/screens/tabs/Tab1Screen.tsx
+++ 04-NavigationApp/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,11 +1,14 @@
 import {Text, View} from 'react-native';
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 
 export const Tab1Screen = () => {
   return (
     <View>
       <HamburgerMenu />
       <Text>Tab1Screen</Text>
+      {myIcon}
     </View>
   );
 };
