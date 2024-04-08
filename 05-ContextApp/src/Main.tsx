ad105329a1a10cbad2d058e43ab15f2548430b74
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-04-08 09:55:49 -0500

    Init ContextApp

---
+++ 05-ContextApp/src/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from './config/'
+
+export const Main = () => {
+    return (
+        <View>
+            <Text>Hello world!</Text>
+        </View>
+    )
+}
