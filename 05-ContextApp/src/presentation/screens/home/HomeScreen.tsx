7a044c8eb617b75fff1477a663eeeb135a506107
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2024-04-10 23:09:12 -0500

    Init Zustand store

--- 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
+++ 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,16 @@
 import { Text, View } from 'react-native'
 import { styles } from '../../../config'
+import { useProfileStore } from '../../store/profile-store';
 
 export const HomeScreen = () => {
+
+    const { name, email } = useProfileStore();
+
     return (
         <View style={styles.container}>
             <Text>HomeScreen</Text>
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.title}>{email}</Text>
         </View>
     )
 }
--- 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
+++ 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,15 @@
 import { Text, View } from 'react-native'
 import { styles } from '../../../config'
+import { useProfileStore } from '../../store/profile-store';
 
 export const ProfileScreen = () => {
+
+  const { name, email } = useProfileStore();
+
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.title}>{email}</Text>
     </View>
   )
 }
