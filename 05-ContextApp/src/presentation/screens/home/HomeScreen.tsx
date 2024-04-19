b64d97455e033bdc111f5bd550cc05b4f6fd96d7
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-04-18 19:24:16 -0500

    Store changes and CounterStore

--- 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
+++ 05-ContextApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,16 +1,19 @@
-import { Text, View } from 'react-native'
-import { styles } from '../../../config'
-import { useProfileStore } from '../../store/profile-store';
+import {Text, View} from 'react-native';
+import {styles} from '../../../config';
+import {useProfileStore} from '../../store/profile-store';
+import {useCounterStore} from '../../store/counter-store';
 
 export const HomeScreen = () => {
+  const name = useProfileStore(state => state.name);
+  const email = useProfileStore(state => state.email);
+  const currentValue = useCounterStore(state => state.currentValue);
 
-    const { name, email } = useProfileStore();
-
-    return (
-        <View style={styles.container}>
-            <Text>HomeScreen</Text>
-            <Text style={styles.title}>{name}</Text>
-            <Text style={styles.title}>{email}</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.title}>{email}</Text>
+      <Text style={styles.title}>Counter: {currentValue}</Text>
+    </View>
+  );
+};
--- 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
+++ 05-ContextApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from 'react-native'
-import { styles } from '../../../config'
-import { useProfileStore } from '../../store/profile-store';
+import {Pressable, Text, View} from 'react-native';
+import {styles} from '../../../config';
+import {useProfileStore} from '../../store/profile-store';
 
 export const ProfileScreen = () => {
+  // const {name, email, changeProfile} = useProfileStore(); // innecesary reloads
 
-  const { name, email } = useProfileStore();
+  const name = useProfileStore((state) => state.name);
+  const email = useProfileStore((state) => state.email);
+  const changeProfile = useProfileStore((state) => state.changeProfile);
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{name}</Text>
       <Text style={styles.title}>{email}</Text>
+
+      <Pressable style={styles.primaryButton} onPress={() => changeProfile("Santi Sánchez", "santi@gmail.com")}>
+        <Text>Change name</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
