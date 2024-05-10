128fa964e87910ab53fd855bb8f573b849e8af8e
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2024-05-10 00:21:18 -0500

    getMovieCastUseCase in general

--- 06-MoviesApp/src/presentation/hooks/useMovie.tsx
+++ 06-MoviesApp/src/presentation/hooks/useMovie.tsx
@@ -1,26 +1,35 @@
 import {useEffect, useState} from 'react';
 import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
 import * as UsesCases from '../../core/use-cases';
-import { FullMovie } from '../../core/models/movie.model';
+import {FullMovie} from '../../core/models/movie.model';
+import {Cast} from '../../core/models/cast.mode';
 
 export const useMovie = (movieId: number) => {
   const [isLoading, setIsLoading] = useState(false);
   const [movie, setMovie] = useState<FullMovie>();
+  const [cast, setCast] = useState<Cast[]>();
 
   useEffect(() => {
     loadlMovie();
   }, []);
 
   const loadlMovie = async (): Promise<FullMovie> => {
-
     setIsLoading(true);
 
-    const movie = await UsesCases.getMovieByIdUseCase(movieDBFetcher, movieId);
-    setMovie(movie);  
-    
+    const fullMoviePromise = UsesCases.getMovieByIdUseCase(
+      movieDBFetcher,
+      movieId,
+    );
+    const castPromise = UsesCases.getMovieCastUseCase(movieDBFetcher, movieId);
+
+    const [movie, cast] = await Promise.all([fullMoviePromise, castPromise]);
+
+    setMovie(movie);
+    setCast(cast);
+
     setIsLoading(false);
 
-    return movie
+    return movie;
   };
 
   return {
